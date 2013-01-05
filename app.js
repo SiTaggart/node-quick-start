24e126d92a0ef16b95738db63ace259a6fcd24bc
Si Taggart <me@simontaggart.com>
2013-01-06 01:04:19 +1100

    still messing around but got a template to render

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var app = require('express').createServer();
-app.use(require('readymade').middleware({root: "public"}));
-app.get('/', function(req, res) {
-    res.send('Simon Taggart');
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('readymade').middleware({root: ''}));
 });
-app.listen(process.env.VCAP_APP_PORT || 3000);
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
